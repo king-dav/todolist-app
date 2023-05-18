7acb0cf172be36d8e163c390cae8b68e71282926
teeyboyz <ovitanventures@gmail.com>
2023-05-18 14:43:48 +0100

    second commit

--- app.js
+++ app.js
@@ -160,6 +160,10 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
  
+ const port = process.env.PORT;
+ if(port == null || port == ""){
+	port = 3000; 
+ }
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
